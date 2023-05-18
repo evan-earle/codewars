1c4da88de9bbc010a294af6ea7fdf5e5cbed962d
Evan Earle <evan.earle@gmail.com>
2023-05-18 19:08:16 -0300

    05/15/23

---
+++ Arrays/script6.js
@@ -0,0 +1,9 @@
+// Write a small function that returns the values of an array that are not odd.
+
+// All values in the array will be integers. Return the good values in the order they are given.
+
+function noOdds(values) {
+  return values.filter((x) => x % 2);
+}
+
+noOdds([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
