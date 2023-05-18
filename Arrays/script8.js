8368f503efdf2480e8df16b9409d2a30820683e5
Evan Earle <evan.earle@gmail.com>
2023-05-18 19:47:43 -0300

    05/18/23

---
+++ Arrays/script8.js
@@ -0,0 +1,15 @@
+// Create a function which compares two probabilities, returning true if the first one is most likely otherwise false.
+
+// For this exercise probability is expressed as two numbers separated by a colon e.g. a probability of 1 in 3 will be 1:3.
+
+// So:
+
+// Input: ('1:3','1:2') - returns false as 1 in 3 is less likely than 1 in 2.
+
+function mostLikely(prob1, prob2) {
+  return eval(prob1.replace(":", "/")) > eval(prob2.replace(":", "/"))
+    ? true
+    : false;
+}
+
+mostLikely("1:3", "1:2");
