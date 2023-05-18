ef5ab226aad918597269b0ed59702d040ba4aae0
Evan Earle <evan.earle@gmail.com>
2023-05-18 18:43:56 -0300

    simplified code

--- Arrays/script5.js
+++ Arrays/script5.js
@@ -10,13 +10,12 @@ function flattenAndSort(array) {
   let flattenedArr = [];
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array[i].length; j++) {
-      if (typeof array[i][j] === "number") {
-        flattenedArr.push(array[i][j]);
-      }
+      flattenedArr.push(array[i][j]);
     }
   }
 
-  return flattenedArr.sort((a, b) => a - b);
+  console.log(flattenedArr.sort((a, b) => a - b));
 }
 
 flattenAndSort([[1], [], [1], [], [], [-1, -2, -1], [0, 3], [1], [2]]);
+flattenAndSort([[3, 2, 1], [4, 6, 5], [], [9, 7, 8]]);
