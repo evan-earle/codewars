ef4bac19b822511070c91968de4be435cf45461a
Evan Earle <evan.earle@gmail.com>
2023-05-20 11:38:46 -0300

    05/20/23

---
+++ Arrays/script12.js
@@ -0,0 +1,24 @@
+// Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+// If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+// All inputs will be valid.
+
+function last(x) {
+  let xArr = x.split(" ");
+  let sortedArr = xArr.sort((a, b) => {
+    let lastA = a.charAt(a.length - 1);
+    let lastB = b.charAt(b.length - 1);
+    if (lastA > lastB) {
+      return 1;
+    } else if (lastA < lastB) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+  return sortedArr;
+}
+
+last("man i need a taxi up to ubud");
+// ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']
