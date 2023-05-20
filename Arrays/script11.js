8fdbea9d5a424c956890d645b78b227688bb4a45
Evan Earle <evan.earle@gmail.com>
2023-05-20 11:23:43 -0300

    edit

--- Arrays/script11.js
+++ Arrays/script11.js
@@ -5,16 +5,20 @@
 // You will be provided a string that lists many boat related items. If any of these items are "Fire" you must spring into action.
 // Change any instance of "Fire" into "~~". Then return the string.
 
-function fireFight(s) {
-  let newArr = s.split(" ");
-  for (let i = 0; i < newArr.length; i++) {
-    if (newArr[i] === "Fire") {
-      newArr[i] = "~~";
-    }
-  }
-  let newString = newArr.toString().replaceAll(",", " ");
+// function fireFight(s) {
+//   let newArr = s.split(" ");
+//   for (let i = 0; i < newArr.length; i++) {
+//     if (newArr[i] === "Fire") {
+//       newArr[i] = "~~";
+//     }
+//   }
+//   let newString = newArr.toString().replaceAll(",", " ");
 
-  return newString;
+//   return newString;
+// }
+
+function fireFight(s) {
+  console.log(s.replaceAll("Fire", "~~"));
 }
 
 fireFight(
