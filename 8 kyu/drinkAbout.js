c57072a46317f180ae4d55c92d0d6da6c452f37a
Evan Earle <evan.earle@gmail.com>
2023-06-19 14:00:00 +0100

    06/19/2023

--- 8 kyu/drinkAbout.js
+++ 8 kyu/drinkAbout.js
@@ -22,4 +22,14 @@
 
 // Answer
 
-function peopleWithAgeDrink(old) {}
+function peopleWithAgeDrink(age) {
+  if (age < 14) {
+    return "drink toddy";
+  } else if (age < 18) {
+    return "drink coke";
+  } else if (age < 21) {
+    return "drink beer";
+  } else {
+    return "drink whisky";
+  }
+}
