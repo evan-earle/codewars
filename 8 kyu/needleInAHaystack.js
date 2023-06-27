b0ed68d614db27edb6fda993b9f8547ca8988768
Evan Earle <evan.earle@gmail.com>
2023-06-27 14:00:00 +0100

    06/27/23

--- 8 kyu/needleInAHaystack.js
+++ 8 kyu/needleInAHaystack.js
@@ -15,4 +15,86 @@
 
 // Answer
 
-function findNeedle(haystack) {}
+// function findNeedle(haystack) {
+//   haystack.forEach((stack, i) => {
+//     if (stack === "needle") {
+//       return `found the needle at position ${i}`;
+//     }
+//   });
+// }
+
+function findNeedle(haystack) {
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] === "needle") {
+      return "found the needle at position " + i;
+    }
+  }
+}
+
+findNeedle([
+  "3",
+  "123124234",
+  undefined,
+  "needle",
+  "world",
+  "hay",
+  2,
+  "3",
+  true,
+  false,
+]);
+findNeedle([
+  "283497238987234",
+  "a dog",
+  "a cat",
+  "some random junk",
+  "a piece of hay",
+  "needle",
+  "something somebody lost a while ago",
+]);
+findNeedle([
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  8,
+  7,
+  5,
+  4,
+  3,
+  4,
+  5,
+  6,
+  67,
+  5,
+  5,
+  3,
+  3,
+  4,
+  2,
+  34,
+  234,
+  23,
+  4,
+  234,
+  324,
+  324,
+  "needle",
+  1,
+  2,
+  3,
+  4,
+  5,
+  5,
+  6,
+  5,
+  4,
+  32,
+  3,
+  45,
+  54,
+]);
