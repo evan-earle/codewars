6757144b88cf7dde8bfbdb90456f9754c270b7a0
Evan Earle <evan.earle@gmail.com>
2023-06-30 14:00:00 +0100

    06/30/23

--- 8 kyu/justCountSheep.js
+++ 8 kyu/justCountSheep.js
@@ -1,11 +1,19 @@
 // If you can't sleep, just count sheep!!
 
-// If you can't sleep, just count sheep!!
-
 // Task:
 // Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e.
 //no negative integers.
 
 // // Answer
 
-var countSheep = function (num) {};
+var countSheep = function (num) {
+  let str = "";
+  for (let i = 1; i <= num; i++) {
+    str += `${i} sheep...`;
+  }
+  return str;
+};
+
+countSheep(5);
+countSheep(0);
+countSheep(3);
