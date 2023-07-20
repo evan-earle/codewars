6fd0c9d95c18f51ec89885c1a3f9ba129c73cce5
Evan Earle <evan.earle@gmail.com>
2023-07-20 14:00:00 +0100

    07/20/23

---
+++ 8 kyu/removeDuplicatesFromList.js
@@ -0,0 +1,11 @@
+// Remove duplicates from list
+
+// Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+// Answer
+
+function distinct(a) {
+  return [];
+}
