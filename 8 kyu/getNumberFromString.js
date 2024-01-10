87484a9d28cfc5f7c10b9e34c7bf26c676f8c340
Evan Earle <evan.earle@gmail.com>
2024-01-10 16:35:00 -0400

    01/10

---
+++ 8 kyu/getNumberFromString.js
@@ -0,0 +1,9 @@
+// Get number from string
+
+// Write a function which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
+
+// Answer
+
+function getNumberFromString(s) {
+  return 0;
+}
