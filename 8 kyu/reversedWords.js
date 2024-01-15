313860c7ca55b200b224e25f76385b54d88cb9c6
Evan Earle <evan.earle@gmail.com>
2024-01-15 11:56:00 -0400

    01/15

--- 8 kyu/reversedWords.js
+++ 8 kyu/reversedWords.js
@@ -10,4 +10,10 @@
 
 // Answer
 
-function reverseWords(str) {}
+function reverseWords(str) {
+  return str.split(" ").reverse().join(" ");
+}
+
+console.log(
+  reverseWords("The greatest victory is that which requires no battle")
+);
